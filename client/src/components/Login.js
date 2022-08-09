38a1ff7782fa9f93b57f893187175a69d1a47cdd
david <davidjhoyjr@gmail.com>
2022-08-09 14:54:52 -0400

    background image

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -6,12 +6,14 @@ import '../css/login.css';
 const Login = () => {
   return (
     <div className = "Login-container">
-        <h1>Login</h1>
+        
         <form className = "form">
         <label className = "label">Username</label>
         <input type = "text" name="username" className = "input"/>
+
         <label className = "label">Password</label>
         <input type = "type" name= "password"className = "input"/>
+
         <button className = "input" id = "Login-button">Login</button>
         </form>
         
