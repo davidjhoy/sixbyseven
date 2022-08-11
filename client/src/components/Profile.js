d4333155bbcf3fc9c0fa1e190bec159da8a80632
david <davidjhoyjr@gmail.com>
2022-08-10 20:37:02 -0400

    Profile styling

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import '..css/profile.css';
+import '../css/profile.css';
 
 
 const Profile = () => {
   return (
     <div className='Profile-container'>
       <div className = 'Profile-header'>
+        <p>header</p>
       </div>
 
       <div className='Profile-body'>
+        <p>body</p>
+        <section className='Profile-body-section'>
+          
+        </section>
       </div>
 
       <div className='Profile-footer'>
+        <p className = "Footer-text">Copyright David Hoy 2022</p>
       </div>
     </div>
   )
